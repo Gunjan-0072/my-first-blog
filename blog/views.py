71c00d9f5ac0ffec8d24a29732ad9a26bf08ec1c
Gunjan-0072 <gunjan.b@amazatic.com>
2022-01-04 17:48:22 +0530

    swap the methos inside views.py

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,11 @@ from django.utils import timezone
 from blog.models import PostBlog
 
 # Create your views here.
-def post_list(request):
-    posts = PostBlog.objects.order_by('created_date')#filter(published_date__lte=timezone.now()) #.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-
 def post_detail(request,pk):
     post = get_object_or_404(PostBlog, pk=pk)
     #posts = PostBlog.objects.order_by('created_date')#filter(published_date__lte=timezone.now()) #.order_by('published_date')
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_list(request):
+    posts = PostBlog.objects.order_by('created_date')#filter(published_date__lte=timezone.now()) #.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
