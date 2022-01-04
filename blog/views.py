65eb14da3b3d470c8ef0931eea71cef97a9ebb2c
Gunjan-0072 <gunjan.b@amazatic.com>
2022-01-04 17:10:39 +0530

    Added new post_details view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import PostBlog
 
@@ -6,3 +6,10 @@ from blog.models import PostBlog
 def post_list(request):
     posts = PostBlog.objects.order_by('created_date')#filter(published_date__lte=timezone.now()) #.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request,pk):
+    post = get_object_or_404(PostBlog, pk=pk)
+    #posts = PostBlog.objects.order_by('created_date')#filter(published_date__lte=timezone.now()) #.order_by('published_date')
+    return render(request, 'blog/post_detail.html', {'post': post})
+
